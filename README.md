ae99480627a1eeaec4e178c77cc662be33ce1a88
Ganesh K Bhat <ganeshsurfs@gmail.com>
2023-02-27 18:46:13 +0530

    [COMMIT] First commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pickler
 npm module implements the python pickle-like binary protocols for serializing and de-serializing a javascript object structure as in python
+
+[Pickle Reference: https://docs.python.org/3/library/pickle.html#module-pickle](https://docs.python.org/3/library/pickle.html#module-pickle)
---
+++ demos/demos.pickle.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * Package: pickler
+ * Author: Ganesh B
+ * Description: 
+ * Install: npm i pickler --save
+ * Github: https://github.com/ganeshkbhat/pickler
+ * npmjs Link: https://www.npmjs.com/package/pickler
+ * File: demos/demos.pickle.js
+ * File Description: 
+ * 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
